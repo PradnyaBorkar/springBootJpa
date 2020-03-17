16fc18b5c541b1abe9d6052216b99bfa34ae2da8
kumb09 <pradnya.borkar@springernature.com>
2020-03-17 13:19:54 +0530

    initial configuration for spring boot jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO EMPLOYEE (first_name, last_name, email) VALUES
+  ('Pradnya', 'Borkar', 'abc@gmail.com'),
+  ('Deja', 'Vu', 'xyz@email.com'),
+  ('Caption', 'America', 'cap@marvel.com');
